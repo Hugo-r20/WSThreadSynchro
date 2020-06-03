de0d867591179394e7348047d5caeb345f58aa94
Hugo ROUTY <hugo.routy@viacesi.fr>
2020-06-03 21:21:29 +0200

    Question 4 C

--- WSThreadSynchro/Question4.cs
+++ WSThreadSynchro/Question4.cs
@@ -9,28 +9,23 @@ namespace WSThreadSynchro
 {
     class Question4
     {
-        private delegate void DELG();
-
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Program start");
             
-            DELG deleg = new DELG(CLpara.method_para);
-            Thread thread = new Thread(deleg.Invoke);
+            Thread thread = new Thread(new ThreadStart(
+                    () =>
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.WriteLine("Work... " + i);
+                            Thread.Sleep(1000);
+                        }
+                    }
+                ));
             thread.Start();
             Console.Read();
-        }
-    }
-
-    static class CLpara
-    {
-        public static void method_para()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("Work... " + i);
-                Thread.Sleep(1000);
-            }
         }
     }
 }
