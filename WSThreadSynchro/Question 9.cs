9bbb66c35ba1a7142c3f8014b3fded2a41b1161b
Hugo ROUTY <hugo.routy@viacesi.fr>
2020-06-04 10:59:53 +0200

    Question 9

---
+++ WSThreadSynchro/Question 9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.CompilerServices;
+
+namespace CorbeilleThreadEtSynchro
+{
+
+
+    class Question9
+    {
+        private delegate void DELG(object state);
+        private static int var = 0;
+        private static Mutex mutex;
+
+        static void Main(string[] args)
+        {
+            mutex = new Mutex(false);
+            DELG deleg = (state) =>
+            {
+                mutex.WaitOne();
+                for (int i = 0; i<3; i++) {
+                    string name_thread = (string)state;
+                    ++var;
+                    Console.WriteLine("Thread -> " + name_thread + " -- var -> " + var.ToString());
+                    Thread.Sleep(2000);
+                }
+                mutex.ReleaseMutex();
+            };
+            Thread t1 = new Thread(deleg.Invoke);
+            Thread t2 = new Thread(deleg.Invoke);
+            t1.Start((object)("T1"));
+            t2.Start((object)("T2"));
+
+            Console.Read();
+        }
+    }
+}
